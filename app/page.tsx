67b5a59052740bd6127aa2830edb34ed1b73d58c
GWorld1 <thunder5613@gmail.com>
2025-02-05 13:11:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Controls } from "@/components/AnimationControls";
 import Scene from "@/components/Scene";
 import TreeFileInput from "@/components/TreeFileInput";
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
     <main className="h-screen w-full overflow-hidden bg-gradient-to-r from-violet-200 to-pink-200">
       <h1 className="text-3xl font-semibold p-4">Algorithm Visualizer</h1>
       <Scene/>
-      
+      <Controls/>
       <TreeFileInput/>
       <footer className="absolute bottom-0 right-0 p-4">
         <p className="text-sm text-gray-500">
